1998e06038badb5e2413fadbe4feb8db76f16aef
NguyetHMM <nguyet332747@gmail.com>
2021-11-20 15:18:24 +0700

    Login - Register - Header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,52 +3,52 @@
 @section('title', 'ログイン')
 
 @section('content')
-  <div class="container">
-    <div class="row">
-      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
-        <div class="card mt-3">
-          <div class="card-body text-center">
-            <h2 class="h3 card-title text-center mt-2">ログイン</h2>
+    <div class="container">
+        <div class="row">
+            <div class="mx-auto col col-12">
+                <h1 class="text-center"><a class="text-dark" 　style="text-decoration: none;" href="/">レシピSNS</a></h1>
+                <h6 class="text-center">レシピ・調理方法を共有ウェブサイト</h6>
+                <div class="d-flex justify-content-center">
+                    <div class="mx-auto" style="width: 30%; height:30%">
+                        <img src="{{ asset('images/login.png') }}" alt="">
+                    </div>
+                    <div class="mx-auto card mt-5" style="width: 50%;">
+                        <div class="card-body">
+                            <h2 class="h3 card-title text-center mt-4">ログイン</h2>
+                            @include('error_card_list')
+                            <div class="card-text ">
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="md-form row mt-4 ">
+                                        <label class="col-sm-2 col-form-label " for="email">メール</label>
+                                        <div class="col-sm-10">
+                                            <input class="form-control" type="text" id="email" name="email" required>
+                                        </div>
+                                    </div>
+                                    <div class="md-form row mt-3 mb-3">
+                                        <label class="col-sm-2 col-form-label" for="password">パスワード</label>
+                                        <div class="col-sm-10">
 
-            <a href="{{ route('login.{provider}', ['provider' => 'google']) }}" class="btn btn-block btn-danger">
-              <i class="fab fa-google mr-1"></i>Googleでログイン
-            </a>
-
-            @include('error_card_list')
-
-            <div class="card-text">
-              <form method="POST" action="{{ route('login') }}">
-                @csrf
-
-                <div class="md-form">
-                  <label for="email">メールアドレス</label>
-                  <input class="form-control" type="text" id="email" name="email" required>
+                                            <input class="form-control" type="password" id="password" name="password"
+                                                required>
+                                        </div>
+                                    </div>
+                                    <input type="hidden" name="remember" id="remember" value="on">
+                                    {{-- <div class="text-left">
+                                        <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                                    </div> --}}
+                                    <div class="text-center">
+                                        <button class="btn btn-block btn-secondary mb-2 " type="submit">ログイン</button>
+                                    </div>
+                                </form>
+                                <div class="mt-2 mb-5 text-center">
+                                    アカウントをお持ちでない。ユーザー登録は<a href="{{ route('register') }}" class="card-text">こちら</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="md-form">
-                  <label for="password">パスワード</label>
-                  <input class="form-control" type="password" id="password" name="password" required>
-                </div>
-
-                <input type="hidden" name="remember" id="remember" value="on">
-
-                <div class="text-left">
-                  <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
-                </div>
-
-                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
-
-              </form>
-
-              <div class="mt-0">
-                <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
-              </div>
-
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 @endsection
