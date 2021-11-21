0078e6e552efa4337a6c68b0235c1502391c41e0
namcoi99 <nam_1999_x@yahoo.com.vn>
2021-11-22 03:48:11 +0700

    home page article list + article detail + save article btn

---
+++ resources/js/components/ArticleSave.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <button
+      class="btn-sm shadow-none p-2"
+      :class="buttonColor"
+      @click="clickSave"
+    >
+      <i class="me-1" :class="buttonIcon"></i>
+      {{ buttonText }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialIsSaved: {
+      type: Boolean,
+      default: false,
+    },
+    authorized: {
+      type: Boolean,
+      default: false,
+    },
+    // endpoint: {
+    //   type: String,
+    // },
+  },
+  data() {
+    return {
+      isSaved: this.initialIsSaved,
+    };
+  },
+  computed: {
+    buttonColor() {
+      return this.isSaved ? "btn btn-success" : "btn btn-outline-success";
+    },
+    buttonIcon() {
+      return this.isSaved ? "fas fa-check" : "far fa-bookmark";
+    },
+    buttonText() {
+      return this.isSaved ? "保存しました" : "保存する";
+    },
+  },
+  methods: {
+    clickSave() {
+      if (!this.authorized) {
+        alert("フォロー機能はログイン中のみ使用できます");
+        return;
+      }
+
+      this.isSaved ? this.unsave() : this.save();
+    },
+    async save() {
+      //   const response = await axios.put(this.endpoint);
+
+      this.isSaved = true;
+    },
+    async unsave() {
+      //   const response = await axios.delete(this.endpoint);
+
+      this.isSaved = false;
+    },
+  },
+};
+</script>
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -3,8 +3,141 @@
 @section('title', '記事詳細')
 
 @section('content')
-  @include('nav')
-  <div class="container">
-    @include('articles.card')
-  </div>
+    @include('layout.header')
+    <div class="container">
+        {{-- @include('articles.card') --}}
+        <div class="card mt-3">
+            <div class="card-header d-flex">
+                <h3 class="card-title text-capitalize">
+                    {{ $article->title }}
+                </h3>
+                @if (Auth::id() === $article->user_id)
+                    <!-- dropdown -->
+                    <div class="ms-auto card-text">
+                        <div class="dropdown">
+                            <a data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <button type="button" class="btn btn-link text-muted m-0 p-2">
+                                    <i class="fas fa-ellipsis-v"></i>
+                                </button>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item" href="{{ route('articles.edit', ['article' => $article]) }}">
+                                    <i class="fas fa-pen mr-1"></i>記事を更新する
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item text-danger" data-bs-toggle="modal"
+                                    data-bs-target="#modal-delete-{{ $article->id }}">
+                                    <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- dropdown -->
+
+                    <!-- modal -->
+                    <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h6 class="modal-title fw-bold">削除確認</h6>
+                                    <button type="button" class="btn close ms-auto" data-bs-dismiss="modal"
+                                        aria-label="閉じる">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <form method="POST" action="{{ route('articles.destroy', ['article' => $article]) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <div class="modal-body">
+                                        {{ $article->title }}を削除します。よろしいですか？
+                                    </div>
+                                    <div class="modal-footer flex-end">
+                                        <a class="btn btn-outline-secondary" data-bs-dismiss="modal">キャンセル</a>
+                                        <button type="submit" class="btn btn-danger">削除する</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- modal -->
+                @else
+                    <article-save class="ms-auto" :authorized='@json(Auth::check())'>
+                    </article-save>
+                @endif
+            </div>
+            <div class="card-body">
+                <div class="d-flex">
+                    <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
+                        <i class="fas fa-user-circle fa-3x mr-1"></i>
+                    </a>
+                    <div class="ms-3">
+                        <div class="fw-bold">
+                            <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
+                                {{ $article->user->name }}
+                            </a>
+                        </div>
+                        <div class="fw-lighter">
+                            {{ $article->created_at->format('Y/m/d H:i') }}
+                        </div>
+                    </div>
+
+                    @if (Auth::id() !== $article->user_id)
+                        <follow-button class="ms-auto"
+                            :initial-is-followed-by='@json($article->user->isFollowedBy(Auth::user()))'
+                            :authorized='@json(Auth::check())'
+                            endpoint="{{ route('users.follow', ['name' => $article->user->name]) }}">
+                        </follow-button>
+                    @endif
+                </div>
+            </div>
+            <img src="https://www.cscassets.com/recipes/wide_cknew/wide_24738.jpg" class="card-img mt-3"
+                alt="recipe image">
+            <div class="card-body">
+                <p class="card-text mt-3">
+                    {!! nl2br(e($article->body)) !!}
+                </p>
+                @if (count($article->tags) > 0)
+                    <label for="">タグ：</label>
+                    @foreach ($article->tags as $tag)
+                        {{-- @if ($loop->first) --}}
+                        <a href="{{ route('tags.show', ['name' => $tag->name]) }}"
+                            class="border p-1 me-1 mt-1 text-muted">
+                            {{ $tag->hashtag }}
+                        </a>
+                        {{-- @endif
+              @if ($loop->last)
+              @endif --}}
+                    @endforeach
+                @endif
+            </div>
+            <div class="card-footer">
+                <article-like :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
+                    :initial-count-likes='@json($article->count_likes)' :authorized='@json(Auth::check())'
+                    endpoint="{{ route('articles.like', ['article' => $article]) }}">
+                </article-like>
+
+            </div>
+            <div class="card-body">
+                {{-- comment list --}}
+                <div class="d-flex my-2 align-items-start">
+                    <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
+                        <i class="fas fa-user-circle fa-2x"></i>
+                    </a>
+                    <div class="ms-2 flex-fill">
+                        <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark fw-bold">
+                            {{ $article->user->name }}
+                        </a>
+                        <span class="ms-2">ii desune</span>
+                    </div>
+                </div>
+                {{-- comment list --}}
+                <form>
+                    <div class="mb-3 d-flex">
+                        <input type="text" class="form-control" id="userComment" placeholder="コメントをする">
+                        <button type="submit" class="btn btn-primary"><i class="fas fa-paper-plane"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
