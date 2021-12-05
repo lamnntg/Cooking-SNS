9542b7736393b67bbe82014545a5cd867e59b305
lamylov3 <lamnguyen.hust@gmail.com>
2021-12-05 15:47:16 +0700

    fix first comment

--- resources/js/app/comment.js
+++ resources/js/app/comment.js
@@ -11,7 +11,16 @@ $("#comment-submit").click(function(event) {
             if (response) {
                 $("#comment").val("");
                 var newComment = response.result;
-                $("#list_comments .comment-list:last-child").after(newComment);
+                var countChild = $("#list_comments").children(".comment-list")
+                    .length;
+
+                if (countChild < 1) {
+                    $("#list_comments").append(newComment);
+                } else {
+                    $("#list_comments .comment-list:last-child").after(
+                        newComment
+                    );
+                }
             }
         },
         error: function(error) {
