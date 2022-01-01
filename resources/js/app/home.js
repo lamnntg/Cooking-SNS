81d59dd33d9fe4bbe373971afbd975996b697823
namcoi99 <nam_1999_x@yahoo.com.vn>
2022-01-02 06:12:19 +0700

    change UI search recipes by tags

--- resources/js/app/home.js
+++ resources/js/app/home.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     //search in input header
-    $("#searchInput").on("keypress", function(e) {
+    $("#searchInput").on("keypress", function (e) {
         var value = $(this)
             .val()
             .toLowerCase();
@@ -11,5 +11,41 @@ $(document).ready(function() {
             window.location.href = origin + "?search=" + value;
         }
     });
-
 });
+
+$('#tags-right-btn').on("click", function (event) {
+    event.preventDefault();
+    $('#tags-content').animate({
+        scrollLeft: "+=400px"
+    }, "slow", function () {
+        toggleScrollButton()
+    });
+});
+
+$('#tags-left-btn').on("click", function (event) {
+    event.preventDefault();
+    $('#tags-content').animate({
+        scrollLeft: "-=400px"
+    }, "slow", function () {
+        toggleScrollButton()
+    });
+});
+
+const tagsContent = document.getElementById("tags-content")
+const tagsLeftBtn = document.getElementById("tags-left-btn")
+const tagsRightBtn = document.getElementById("tags-right-btn")
+
+const toggleScrollButton = () => {
+    if (tagsContent.scrollLeft == 0) {
+        tagsLeftBtn.style.display = "none"
+    } else {
+        tagsLeftBtn.style.display = "block"
+    }
+    if (tagsContent.scrollWidth == tagsContent.offsetWidth + tagsContent.scrollLeft) {
+        tagsRightBtn.style.display = "none"
+    } else {
+        tagsRightBtn.style.display = "block"
+    }
+}
+
+toggleScrollButton()
